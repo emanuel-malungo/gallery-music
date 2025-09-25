f9f43fbb6324d33a99f42e13ecbcb8960ce383a9
emanuel-malungo <emanuelmalungo856@gmail.com>
2025-09-25 06:12:30 -0400

    feat: add initial pages for dashboard and artist sections

---
+++ src/app/(listener)/loading.tsx
@@ -0,0 +1,8 @@
+// app/dashboard/loading.tsx
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <p className="text-gray-500 animate-pulse">Carregando...</p>
+    </div>
+  );
+}
---
+++ src/app/artist/send-music/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SendMusic() {
+  return (
+    <div>Send Music</div>
+  )
+}
